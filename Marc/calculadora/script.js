617a90cc8ce23db1528eb553dfb0ec5d3f188238
marcosokita181@gmail.com <marcosokita181@gmail.com>
2023-07-06 14:04:08 -0300

    Tudo Pronto

--- Marc/calculadora/script.js
+++ Marc/calculadora/script.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+function inserir(valor){
+   var calc = document.getElementById("calc")
+   document.getElementById("calc").innerHTML+= valor
+}
+function apagar(){
+   var calc = document.getElementById("calc").innerHTML
+   document.getElementById("calc").innerHTML = calc.substring(0,calc.length-1)
+}
+function apagarTudo(){
+   var calc = document.getElementById("calc")
+   calc.innerHTML = ""
+}
+function calcular(){
+   var calc = document.getElementById("calc").innerHTML
+   document.getElementById("calc").innerHTML = eval(calc)
+}
